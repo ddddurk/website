1589947d28f01f08698141c7e01bc87c0c5ab29e
ddddurk <ddddurk@proton.me>
2023-05-06 13:17:42 -0400

    feat(src/app): update Layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,29 @@
 import "tailwindcss/tailwind.css";
 
+import {
+  Footer,
+  GridBasic,
+  Header,
+  Navigation,
+  OverlayGradient,
+  OverlayGrid
+} from "@src/components";
 import type { ReactNode } from "react";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <html>
-      <body>{children}</body>
+    <html lang="en">
+      <GridBasic
+        as="body"
+        className="min-h-screen grid-rows-[auto,auto,1fr,auto] bg-white p-4 text-gray-800 antialiased md:p-6 md:text-lg [&_*]:transition-all"
+      >
+        <OverlayGradient />
+        <OverlayGrid />
+        <Header />
+        <Navigation />
+        {children}
+        <Footer />
+      </GridBasic>
     </html>
   );
 };
