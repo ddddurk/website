a89f3a5f24b81ca8fff9a990e1ee1ae1159d3bdd
ddddurk <ddddurk@proton.me>
2023-05-06 11:12:09 -0400

    chore(src/hooks): create useViews.ts

---
+++ src/hooks/useViews.ts
@@ -0,0 +1,20 @@
+"use client";
+
+import { fetcher } from "@src/lib";
+import { useEffect } from "react";
+import useSwr from "swr";
+
+export const useViews = (
+  slug: string,
+  options?: { post?: boolean }
+): [string] => {
+  const API_URL = `/api/blog/${slug}/views`;
+
+  const { data } = useSwr(API_URL, fetcher);
+
+  useEffect(() => {
+    if (options?.post) fetch(API_URL, { method: "POST" });
+  }, []);
+
+  return [data ? data.views.toLocaleString() : "0"];
+};
