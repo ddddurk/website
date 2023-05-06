a65d0fb52d4e2445302f48873d2c7a6bf69375d1
ddddurk <ddddurk@proton.me>
2023-05-06 00:57:32 -0400

    chore: add @ddddurk/colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,19 @@
+import ddddurkColors from "@ddddurk/colors/colors/colors.json";
 import type { Config } from "tailwindcss";
+import tailwindColors from "tailwindcss/colors";
 
 const config: Config = {
-  content: ["./content/**/*.mdx", "./src/**/*.tsx"]
+  content: ["./content/**/*.mdx", "./src/**/*.tsx"],
+  theme: {
+    colors: {
+      black: ddddurkColors.black.black,
+      cyan: ddddurkColors.cyan,
+      gray: tailwindColors.stone,
+      green: ddddurkColors.green,
+      pink: ddddurkColors.pink,
+      white: ddddurkColors.white.white
+    }
+  }
 };
 
 export default config;
