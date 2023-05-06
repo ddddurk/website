ca4a293bc4dbc8a31e5552c44f587c4f77b34acb
ddddurk <ddddurk@proton.me>
2023-05-06 12:47:51 -0400

    chore(src/lib): create links

---
+++ src/lib/links.ts
@@ -0,0 +1,8 @@
+export const LINKS_DDDSTACK = "https://github.com/dddstack";
+
+export const LINKS_EMAIL = "mailto: ddddurk@proton.me";
+
+export const LINKS_GITHUB = "https://github.com/ddddurk";
+
+export const LINKS_LINKEDIN =
+  "https://www.linkedin.com/in/ddddurk";
