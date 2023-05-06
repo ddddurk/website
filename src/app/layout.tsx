84bcf6d78af95e5d905c953d9915bc6429c02f0e
ddddurk <ddddurk@proton.me>
2023-05-05 23:54:01 -0400

    chore: create next app

---
+++ src/app/layout.tsx
@@ -0,0 +1,11 @@
+import type { ReactNode } from "react";
+
+const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  );
+};
+
+export default Layout;
