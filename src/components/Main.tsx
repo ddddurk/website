0be13f2637d8719e78e6c22f6efa4eb0385b0452
ddddurk <ddddurk@proton.me>
2023-05-12 02:19:53 -0400

    feat(components): update workComponents/Colors.tsx, Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -10,7 +10,7 @@ const Main = ({ children }: MainProps) => {
   return (
     <PolymorphGridComplex
       as="main"
-      className="!col-start-0 !col-span-3 [&_h1]:text-gray-900 [&_h1]:text-3xl [&_h1]:sm:text-4xl [&_h1]:md:text-5xl [&_h1]:font-bold [&_pre]:bg-gray-800 [&_pre]:text-sm [&_pre]:md:text-base [&_pre]:p-4 [&_pre]:md:px-6 [&_pre]:rounded-xl [&_h2]:text-xl [&_h2]:font-medium [&_h2]:md:text-2xl [&_h2]:text-gray-900"
+      className="!col-start-0 !col-span-3 [&_h1]:text-gray-900 [&_h1]:text-3xl [&_h1]:sm:text-4xl [&_h1]:md:text-5xl [&_h1]:font-bold [&_pre]:bg-gray-800 [&_pre]:text-sm [&_pre]:md:text-base [&_pre]:p-4 [&_pre]:md:px-6 [&_pre]:rounded-xl [&_pre]:overflow-x-scroll"
     >
       {children}
     </PolymorphGridComplex>
